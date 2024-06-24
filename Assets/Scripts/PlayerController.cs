05747a1cd714c18d21cba82d80b889b0c530e0a3
Graced <simone.bisato37@gmail.com>
2024-06-24 12:05:30 +0200

    dev 0.01.02
    fix class playercontroller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,86 +5,82 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public InputActionAsset InputActions;
+    // KEYBOARD
+    
+    [SerializeField] private float forwardSpeed;
+    [SerializeField] private float strafeSpeed;
+    [SerializeField] private float hoverSpeed;
 
-    //[SerializeField] private float speed;
-    [SerializeField] private float rotationSpeed;
+    // time how fast the ship going from 0 to 1, that means ( 1/2)
+    [SerializeField] private float forwardAcceleration;
+    [SerializeField] private float strafeAcceleration;
+    [SerializeField] private float hoverAcceleration;
 
-    private InputAction rollLeft;
-    private InputAction rollRight;
+    private float activeForwardSpeed;
+    private float activeStrafeSpeed;
+    private float activeHoverSpeed;
 
-    private float yaw;
-    private float pitch;
+
+    // MOUSE 
+
+    [SerializeField] private float lookRateSpeed;
+    [SerializeField] private float rollSpeed;
+    [SerializeField] private float rollAcceleration;
+
+    // save the mouse position. where the mouse is on the screen
+    private Vector2 lookInput;
+
+    // how far is the mouse from the center'screen ( need to know how far to move and rotate )
+    private Vector2 screenCenter;
+
+    // howw far the mouse is at a particular time
+    private Vector2 mouseDistance;
+
+    private float rollInput;
+
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        screenCenter.x = Screen.width * 0.5f;
+        screenCenter.y = Screen.height * 0.5f;
+
+        // Keep the mouse inside of the screen game
+        Cursor.lockState = CursorLockMode.Confined;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //Get Mouse Input
-        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+        // Where the mouse is
+        lookInput.x = Input.mousePosition.x;
+        lookInput.y = Input.mousePosition.y;
 
-        //Update yaw and pitch angles
-        yaw += mouseX;
-        pitch -= mouseY; // inverted axis
-        pitch = Mathf.Clamp(pitch, -90f, 90f);
+        // Distance from the center 
+        mouseDistance.x = (lookInput.x - screenCenter.x) / screenCenter.y; // use screenCenter.y to don't go out of the screen
+        mouseDistance.y = (lookInput.y - screenCenter.y) / screenCenter.y;
 
-        //Apply rotation to the camera
-        transform.eulerAngles = new Vector3(pitch, yaw, 0f);
-    }
+        mouseDistance = Vector2.ClampMagnitude(mouseDistance, 1f); // Range, not matter how much we move our mouse, if the value would be more than on, i'll never be able to move beyond it
 
-    private void OnEnable()
-    {
-        InputActionMap playerActionMap = InputActions.FindActionMap("Player");
-        
-        rollLeft = playerActionMap.FindAction("RollLeft");
-        rollRight = playerActionMap.FindAction("RollRight");
+        // Rotate the ship around along the forward axis ( orientate the ship ) 
+        rollInput = Mathf.Lerp(rollInput, Input.GetAxisRaw("Roll"), rollAcceleration * Time.deltaTime);
 
 
-        rollLeft.performed += OnMove;
-        rollRight.performed += OnMove;
+        // Moving Ship
+        transform.Rotate(-mouseDistance.y * lookRateSpeed * Time.deltaTime, mouseDistance.x * lookRateSpeed * Time.deltaTime, rollInput * rollSpeed * Time.deltaTime, Space.Self);
 
-        rollLeft.Enable();
-        rollRight.Enable();
-  
-    }
+        activeForwardSpeed = Mathf.Lerp(activeForwardSpeed, Input.GetAxisRaw("Vertical") * forwardSpeed, forwardAcceleration * Time.deltaTime);
+        activeStrafeSpeed = Mathf.Lerp(activeStrafeSpeed, Input.GetAxisRaw("Horizontal") * strafeSpeed, strafeAcceleration * Time.deltaTime);
+        activeHoverSpeed = Mathf.Lerp(activeHoverSpeed, Input.GetAxisRaw("Hover") * hoverSpeed, hoverAcceleration * Time.deltaTime);
 
-    private void OnMove(InputAction.CallbackContext context)
-    {
-        float input = context.ReadValue<float>();
-
-        Debug.Log(input);
+        transform.position += transform.forward * activeForwardSpeed * Time.deltaTime;
+        transform.position += (transform.right * activeStrafeSpeed * Time.deltaTime) + (transform.up * activeHoverSpeed * Time.deltaTime);
+       
     }
 
     
-
-    public float GetRollLeftMovement()
-    {
-        return rollLeft.ReadValue<float>();
-
-    }
-
-    public float GetRollRightMovement()
-    {
-        return rollRight.ReadValue<float>();
-
-    }
-
     
-
-    private void OnDisable()
-    {
-
-        rollLeft.Disable();
-        rollRight.Disable();
-
-        rollLeft.performed -= OnMove;
-        rollRight.performed -= OnMove;
-  
-    }
 }
